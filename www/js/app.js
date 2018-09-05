452b840b53ba16813829f7e5662ade5712891705
Joel Tavarez <joeltavarezestevez@gmail.com>
2018-09-05 12:58:21 -0400

    First Commit

---
+++ www/js/app.js
@@ -0,0 +1,325 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+
+    // Enable to debug issues.
+    // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+    var notificationOpenedCallback = function(jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+
+    /*window.plugins.OneSignal
+      .startInit("101f4c41-0c4a-40a7-9845-4ee3c926e827")
+      .handleNotificationOpened(notificationOpenedCallback)
+      .endInit();*/
+      
+    // Sync hashed email if you have a login system or collect it.
+    //   Will be used to reach the user at the most optimal time of day.
+    // window.plugins.OneSignal.syncHashedEmail(userEmail);
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'index.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('contacto', {
+    url: '/app/contacto',
+    templateUrl: 'templates/contacto.html',
+    controller: 'ContactoCtrl'
+  })
+
+    .state('facebook', {
+    url: '/app/facebook',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/facebook.html'
+      }
+    }
+  })
+  
+// setup an abstract state for the tabs directive
+/*  .state('comunidad', {
+    url: '/comunidad',
+    abstract: true,
+    templateUrl: 'templates/comunidad.html'
+  })*/
+
+/*      .state('comunidad', {
+      url: '/comunidad',
+      templateUrl: 'templates/comunidad.html',
+      controller: 'ComunidadCtrl'
+    })
+*/
+  .state('comunidad', {
+    url: '/app/comunidad',
+    templateUrl: 'templates/comunidad.html',
+    controller: 'ComunidadCtrl'
+  })
+  // Each tab has its own nav history stack:
+
+  .state('comunidad.filosofia', {
+    url: '/filosofia',
+    views: {
+      'tab-filosofia': {
+        templateUrl: 'templates/tab-filosofia.html',
+        controller: 'FilosofiaCtrl'
+      }
+    }
+  })
+
+  .state('comunidad.apadavi', {
+    url: '/apadavi',
+    views: {
+      'tab-apadavi': {
+        templateUrl: 'templates/tab-apadavi.html',
+        controller: 'ApadaviCtrl'
+      }
+    }
+  })
+
+  .state('comunidad.sociedadhonor', {
+    url: '/sociedadhonor',
+    views: {
+      'tab-sociedadhonor': {
+        templateUrl: 'templates/tab-sociedadhonor.html',
+        controller: 'SociedadHonorCtrl'
+      }
+    }
+  })    
+
+  .state('comunidad.promociones', {
+    url: '/promociones',
+    views: {
+      'tab-promociones': {
+        templateUrl: 'templates/tab-promociones.html',
+        controller: 'PromocionesCtrl'
+      }
+    }
+  })
+
+  .state('comunidad.promociones-detalle', {
+    url: '/promociones/:Id',
+    views: {
+      'tab-promociones': {
+        templateUrl: 'templates/tab-promociones-detail.html',
+        controller: 'PromocionDetailCtrl'
+      }
+    }
+  })  
+
+  .state('calendario', {
+      url: '/app/calendario',
+      templateUrl: 'templates/calendario.html',
+      controller: 'CalendarioCtrl'            
+    })
+  
+  .state('horarios', {
+    url: '/app/horarios',
+    templateUrl: 'templates/horarios.html',
+    controller: 'HorariosCtrl'
+  })
+
+  .state('circulares', {
+    url: '/app/circulares',
+    templateUrl: 'templates/circulares.html',
+    controller: 'CircularesCtrl'
+  }) 
+
+     .state('circulares-detalle', {
+    url: '/app/circulares/:Id',
+    templateUrl: 'templates/circular-detail.html',
+    controller: 'CircularDetailCtrl'
+  })   
+
+  .state('horarios-detalle', {
+    url: '/app/horarios/:Id',
+    templateUrl: 'templates/horario-detail.html',
+    controller: 'HorarioDetailCtrl'
+  })   
+  
+  .state('noticias', {
+    url: '/app/noticias',
+    templateUrl: 'templates/noticias.html',
+    controller: 'FeedCtrl'
+  })
+  
+    .state('dashboard', {
+      url: '/app/dashboard',
+      templateUrl: 'templates/dashboard.html',
+      controller: 'DashboardController'
+    })
+
+    .state('institucion', {
+      url: '/app/institucion',
+      templateUrl: 'templates/institucion.html',
+      controller: 'ComunidadCtrl'
+    });
+    
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/dashboard');
+})
+
+/**
+ * Handle Google Maps API V3+
+ */
+// - Documentation: https://developers.google.com/maps/documentation/
+.directive("appMap", function ($window, $rootScope) {
+    return {
+        restrict: "E",
+        replace: true,
+        template: "<div></div>",
+        scope: {
+            center: "=",        // Center point on the map (e.g. <code>{ latitude: 10, longitude: 10 }</code>).
+            markers: "=",       // Array of map markers (e.g. <code>[{ lat: 10, lon: 10, name: "hello" }]</code>).
+            width: "@",         // Map width in pixels.
+            height: "@",        // Map height in pixels.
+            zoom: "@",          // Zoom level (one is totally zoomed out, 25 is very much zoomed in).
+            mapTypeId: "@",     // Type of tile to show on the map (roadmap, satellite, hybrid, terrain).
+            panControl: "@",    // Whether to show a pan control on the map.
+            zoomControl: "@",   // Whether to show a zoom control on the map.
+            scaleControl: "@"   // Whether to show scale control on the map.
+        },
+        link: function (scope, element, attrs) {
+            var toResize, toCenter;
+            //var map;
+            var infowindow;
+            var currentMarkers;
+   	        var callbackName = 'InitMapCb';
+
+   			// callback when google maps is loaded
+			$window[callbackName] = function() {
+				console.log("map: init callback");
+				createMap();
+				updateMarkers();
+				};
+
+			if (!$window.google || !$window.google.maps ) {
+				console.log("map: not available - load now gmap js");
+				loadGMaps();
+				}
+			else
+				{
+				console.log("map: IS available - create only map now");
+				createMap();
+				}
+			function loadGMaps() {
+				console.log("map: start loading js gmaps");
+				var script = $window.document.createElement('script');
+				script.type = 'text/javascript';
+				script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyB1Td_pMED3L7rLo_EBKFf3orhjoQE3wHE&v=3.exp&callback=InitMapCb';
+				$window.document.body.appendChild(script);
+				}
+
+			function createMap() {
+				console.log("map: create map start");
+				var mapOptions = {
+					zoom: 16,
+					center: new google.maps.LatLng(19.4592846, -70.6593584),
+					mapTypeId: google.maps.MapTypeId.ROADMAP,
+					panControl: false,
+          fullscreenControl: false,
+					zoomControl: true,
+					mapTypeControl: false,
+					scaleControl: false,
+					streetViewControl: false,
+					navigationControl: false,
+					//disableDefaultUI: true,
+					//overviewMapControl: true,
+                    draggable:true
+					};
+				if (!($rootScope.map instanceof google.maps.Map)) {
+					console.log("map: create map now as not already available ");
+					$rootScope.map = new google.maps.Map(element[0], mapOptions);
+          // EDIT Added this and it works on android now
+          // Stop the side bar from dragging when mousedown/tapdown on the map
+          google.maps.event.addDomListener(element[0], 'mousedown', function(e) {
+            e.preventDefault();
+            return false;
+            });
+					infowindow = new google.maps.InfoWindow(); 
+					}
+				}
+
+			scope.$watch('markers', function() {
+				updateMarkers();
+				});
+
+			// Info window trigger function 
+			function onItemClick(pin, label, datum, url) { 
+				// Create content  
+				var contentString = "<b>Instituto Leonardo Da Vinci</b><br/> Carretera Don Pedro, Esq. Calle El Guano, Km.1, Santiago, Rep. Dom.<br>Tel. 809-734-1535<br>Correo Electrónico: info@leonardo-da-vinci.edu.do";
+				// Replace our Info Window's content and position
+				infowindow.setContent(contentString);
+				infowindow.setPosition(pin.position);
+				infowindow.open($rootScope.map)
+				google.maps.event.addListener(infowindow, 'closeclick', function() {
+					//console.log("map: info windows close listener triggered ");
+					infowindow.close();
+					});
+				} 
+
+			function markerCb(marker, member, location) {
+			    return function() {
+					//console.log("map: marker listener for " + member.name);
+					var href="http://maps.apple.com/?q="+member.lat+","+member.lon;
+					$rootScope.map.setCenter(location);
+					onItemClick(marker, member.name, member.date, href);
+					};
+				}
+
+			// update map markers to match scope marker collection
+			function updateMarkers() {
+				if ($rootScope.map && scope.markers) {
+					// create new markers
+					//console.log("map: make markers ");
+					currentMarkers = [];
+					var markers = scope.markers;
+					if (angular.isString(markers)) markers = scope.$eval(scope.markers);
+					for (var i = 0; i < markers.length; i++) {
+						var m = markers[i];
+						var loc = new google.maps.LatLng(m.lat, m.lon);
+						var mm = new google.maps.Marker({ position: loc, map: $rootScope.map, title: m.name, icon: 'img/davinci-logo-marker.png' });
+						//console.log("map: make marker for " + m.name);
+						google.maps.event.addListener(mm, 'click', markerCb(mm, m, loc));
+						currentMarkers.push(mm);
+            console.log('marker added!');
+						}
+					}
+				}
+
+			// convert current location to Google maps location
+			function getLocation(loc) {
+				if (loc == null) return new google.maps.LatLng(19.4589846, -70.6593584);
+				if (angular.isString(loc)) loc = scope.$eval(loc);
+				return new google.maps.LatLng(loc.lat, loc.lon);
+				}
+
+			} // end of link:
+		}; // end of return
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,561 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+}])
+
+.controller('ComunidadCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+
+ $scope.openMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+}])
+
+.controller('ApadaviCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+
+ $scope.openMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+}])
+
+.controller('FilosofiaCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+    console.log('Aqui Toy!');
+ $scope.openMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+  
+}])
+
+.controller('SociedadHonorCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+
+ $scope.openMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+  
+}])
+
+.controller('CircularesCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+
+    $scope.circulares = [];
+    $scope.url = "http://leonardo-da-vinci.edu.do/";
+    $http.get('http://leonardo-da-vinci.edu.do/circulares.json').success(function(data) {    
+        $scope.circulares = data;
+    });
+}])
+
+.controller('PromocionesCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+
+    $scope.promociones = [];
+    
+    $http.get('data/promociones.json').success(function(data) {
+        $scope.promociones = data;
+    });
+}])
+
+.controller('HorariosCtrl', ['$scope', '$http', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $ionicModal, $ionicSideMenuDelegate) {
+
+    $scope.openMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+
+    $scope.horarios = [];
+    $scope.url = "http://leonardo-da-vinci.edu.do/";
+    $http.get('http://leonardo-da-vinci.edu.do/horarios.json').success(function(data) {
+        $scope.horarios = data;
+        console.log($scope.horarios[0].niveles);
+    });
+
+    $ionicModal.fromTemplateUrl('templates/modal-fotoHorario.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modalHorario = modal;
+    }); 
+
+    $scope.openModalHorario = function() {
+        $scope.modalHorario.show();
+    };
+
+    $scope.closeModalHorario = function() {
+        $scope.modalHorario.hide();
+    };    
+
+    $scope.toggleGroup = function(nivel) {
+        nivel.show = !nivel.show;
+    };
+    $scope.isGroupShown = function(nivel) {
+        return nivel.show;
+    };    
+}])
+
+.controller('PromocionDetailCtrl', ['$scope', '$http', '$stateParams', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $stateParams, $ionicModal, $ionicSideMenuDelegate) {
+
+    $scope.openMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+
+    $scope.promociones = [];
+    $scope.promocion = {};
+
+    $http.get('data/promociones.json').success(function(data) {
+        $scope.promociones = data;
+        for (var i = 0; i < $scope.promociones.length; i++) {
+            if ($scope.promociones[i].id === parseInt($stateParams.Id)) {
+                $scope.promocion = $scope.promociones[i];
+            }
+        }        
+    });
+
+    $ionicModal.fromTemplateUrl('templates/modal-fotoCuadro.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modalCuadro = modal;
+    });
+
+    $ionicModal.fromTemplateUrl('templates/modal-fotoCollage.html', {
+        scope: $scope,
+        animation: 'slide-in-up',
+        controller: 'PromocionDetailCtrl'
+    }).then(function(modal) {
+        $scope.modalCollage = modal;
+    });    
+
+    $scope.openModalCuadro = function() {
+        $scope.modalCuadro.show();
+    };
+
+    $scope.closeModalCuadro = function() {
+        $scope.modalCuadro.hide();
+    };
+
+    $scope.openModalCollage = function() {
+        $scope.modalCollage.show();
+    };
+
+    $scope.closeModalCollage= function() {
+        $scope.modalCollage.hide();
+    };
+
+    $scope.toggleGroup = function(curso) {
+        curso.show = !curso.show;
+    };
+    $scope.isGroupShown = function(curso) {
+        return curso.show;
+    };    
+}])
+
+.controller('HorarioDetailCtrl', ['$scope', '$http', '$stateParams', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $stateParams, $ionicModal, $ionicSideMenuDelegate) {
+
+    $scope.openMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+
+    $scope.horarios = [];
+    $scope.horario = {};
+    $scope.url = "http://leonardo-da-vinci.edu.do/";
+    $http.get('http://leonardo-da-vinci.edu.do/horarios.json').success(function(data) {
+        $scope.horarios = data;
+        for (var i = 0; i < $scope.horarios[0].niveles.length; i++) {
+            for(var j = 0; j < $scope.horarios[0].niveles[i].horarios.length; j++)
+            if ($scope.horarios[0].niveles[i].horarios[j].id === parseInt($stateParams.Id)) {
+                $scope.horario = $scope.horarios[0].niveles[i].horarios[j];
+                console.log($scope.horario);
+            }
+        }        
+    });
+
+    $ionicModal.fromTemplateUrl('templates/modal-fotoHorario.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modalHorario = modal;
+    }); 
+
+    $scope.openModalHorario = function() {
+        $scope.modalHorario.show();
+    };
+
+    $scope.closeModalHorario = function() {
+        $scope.modalHorario.hide();
+    };
+}])
+
+
+.controller('CircularDetailCtrl', ['$scope', '$http', '$stateParams', '$ionicModal', '$ionicSideMenuDelegate', function ($scope, $http, $stateParams, $ionicModal, $ionicSideMenuDelegate) {
+
+    $scope.openMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+
+    $scope.circulares = [];
+    $scope.circular = {};
+    $scope.url = "http://leonardo-da-vinci.edu.do/";
+    $http.get('http://leonardo-da-vinci.edu.do/circulares.json').success(function(data) {
+        $scope.circulares = data;
+        for (var i = 0; i < $scope.circulares.length; i++) {
+            if ($scope.circulares[i].id === parseInt($stateParams.Id)) {
+                $scope.circular = $scope.circulares[i];
+                console.log($scope.circular);
+            }
+        }        
+    });
+
+    $ionicModal.fromTemplateUrl('templates/modal-fotoCircular.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modalCircular = modal;
+    });
+
+    $scope.openModalCircular = function() {
+        $scope.modalCircular.show();
+    };
+
+    $scope.closeModalCircular = function() {
+        $scope.modalCircular.hide();
+    };
+}])
+.controller('ContactoCtrl', ['$scope','$ionicPlatform', '$location', '$ionicModal', '$timeout', '$http', '$ionicSideMenuDelegate', function($scope, $ionicPlatform, $location, $ionicModal, $timeout, $http, $ionicSideMenuDelegate) {
+
+
+
+  // Form data for the login modal
+  $scope.contact = {};
+
+ $scope.openMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/mensaje.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeMensaje = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.mensaje = function() {
+    $scope.modal.show();
+    console.log('modal open');
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.sendContact = function() {
+    console.log('Sending Message', $scope.contact);
+    $http.post('http://www.leonardo-da-vinci.edu.do/sendmail.php', $scope.contact)
+    .success(
+      function(data){
+        $scope.response = data;
+        $scope.closeMensaje();
+        console.log('Message sended');
+        console.log($scope.response);
+      })
+    .error(
+      function(data){
+        $scope.response = data;
+        $scope.closeMensaje();
+        console.log('Error sending message', $scope.response);
+      })
+  }
+    
+	// init gps array
+    $scope.whoiswhere = [];
+    $scope.basel = { lat: 19.4587846, lon: -70.6593584 };
+
+    // some points of interest to show on the map
+    // to be user as markers, objects should have "lat", "lon", and "name" properties
+    $scope.whoiswhere = [
+        { "name": "Instituto Leonardo Da Vinci", "lat": $scope.basel.lat, "lon": $scope.basel.lon, "icon": "img/davinci-logo-marker.png" }
+	];
+}])
+
+.controller("FeedCtrl", function($http, $scope, $ionicSideMenuDelegate) {
+ 
+    $scope.rss = [];
+    $scope.entries = [];
+    $scope.init = function() {
+        $http.jsonp("http://www.leonardo-da-vinci.edu.do/feed/json", {cache: true, params: {callback: 'JSON_CALLBACK'}})
+            .success(function(data) {
+                console.log(data);
+                $scope.rss = data;
+                angular.forEach($scope.rss, function(entry) {
+                    entry.publishedDate = new Date(entry.date);
+                    $scope.entries.push(entry);
+                })
+
+                console.log($scope.entries);
+               /* $scope.rssTitle = data.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;*/
+            })
+            .error(function(data) {
+                console.log("ERROR: " + data);
+            });
+    }
+    
+ /*
+  * Metodo doRefresh() - Sirve para refrescar el listado de los usuarios almacenados en la BD. 
+  * (El Ion-Content debe tene la propiedad scroll="true").
+  */
+  $scope.doRefresh = function(){
+    //Ejecutar el metodo init.
+    $scope.init();
+    //Completar refresh.
+    $scope.$broadcast('scroll.refreshComplete');
+  }
+    
+    $scope.openURL = function(link) {
+        try {
+            window.open(link, '_system', 'location=no');
+        } catch (err) {
+            alert(err);
+        }
+    }
+
+     $scope.openMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+ 
+})
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('DashboardController', function($scope, $state, $ionicHistory) {
+    $scope.notas = function (){
+      $ionicHistory.nextViewOptions({
+        disableBack: true
+      });        
+      $state.go('app.notas');
+    }
+})
+
+.controller('CalendarioCtrl', function($scope, $ionicSideMenuDelegate, $http) {
+
+ $scope.openMenu = function () {
+    $ionicSideMenuDelegate.toggleLeft();
+  }
+
+  $scope.calendario = [];
+
+  $http.get('http://leonardo-da-vinci.edu.do/calendario.json').success(function(data) {
+    $scope.calendario = data;
+  });  
+    
+    console.log($scope.calendario);
+})
+
+.controller('NotasCtrl', function($scope) {
+    
+    $scope.alumno = "PICHARDO ESTEVEZ JUAN JOSE";
+    $scope.matricula = "2015-3616";
+    $scope.curso = "DECIMO";
+    $scope.nivel = "SECUNDARIO";
+    $scope.seccion = "A";
+    $scope.ano_escolar = "2015-2016";
+    
+    $scope.data = [
+        {
+            asignatura: "LENGUA ESPANOLA",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "LENG. EXTR. (INGLES)",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "LENG. EXTR. (FRANCES)",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "MATEMATICA",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "CIENCIAS SOCIALES",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "CIENCIAS NATURALES",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "EDUCACION MORAL Y CIVICA",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "EDUCACION ARTISTICA",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "CIENCIAS NATURALES",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "EDUCACION FISICA",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "LABORATORIO DE INFORMATICA",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        },
+        {
+            asignatura: "FORMACION INTEGRAL HUMANA Y RELIGIOSA",
+            ev1: 85,
+            ev2: 90,
+            ev3: 80,
+            ev4: 85,
+            promsem: 85,
+            prom70: 60,
+            prusem: 90,
+            pru30: 27,
+            cfs: 87,
+            asi: 100
+        }  
+    ];
+});
