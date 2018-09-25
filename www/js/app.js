06c3b8ab8b8ca9a0f7f646f5b8e6a79b0359ecca
Joel Tavarez <joeltavarezestevez@gmail.com>
2018-09-25 12:45:46 -0400

    Fixed modals image on iOS

--- www/js/app.js
+++ www/js/app.js
@@ -22,7 +22,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     // Enable to debug issues.
     // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
-   /* var notificationOpenedCallback = function(jsonData) {
+    var notificationOpenedCallback = function(jsonData) {
       console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
     };
 
@@ -32,7 +32,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       .endInit();
     // Sync hashed email if you have a login system or collect it.
     //   Will be used to reach the user at the most optimal time of day.
-    // window.plugins.OneSignal.syncHashedEmail(userEmail);*/
+    // window.plugins.OneSignal.syncHashedEmail(userEmail);
   });
 })
 
